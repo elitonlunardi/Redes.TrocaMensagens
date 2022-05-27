ca9f289672b9281f99311c8996c78ab7b84e6598
Eliton Lunardi <lunardi.eliton@gmail.com>
2022-05-27 20:34:23 -0300

    Socket utilizando using para correto dispose.

--- Redes.TrocaMensagens.Comunicacao/LarcClient.cs
+++ Redes.TrocaMensagens.Comunicacao/LarcClient.cs
@@ -11,52 +11,54 @@ public class LarcClient : ILarcClient
     private const string USUARIO_APLICACAO = "6806:epftj";
     private const string USER_COMUNICACAO = "4038";
     private const string TODOS_USUARIOS = "0";
-    
+
     private readonly IPEndPoint _endpointLocal;
     private readonly IPEndPoint _endpointLarcTcp;
     private readonly IPEndPoint _endPointLarcUdp;
-    
+
     public LarcClient()
     {
         int portaUdp = 1011;
         int portaTcp = 1012;
         var ipHostLarcInf = Dns.GetHostEntry("larc.inf.furb.br");
         var ipAdress = ipHostLarcInf.AddressList[0].MapToIPv4();
-        
+
         var ipAddressLocal = Dns.GetHostEntry(Dns.GetHostName()).AddressList[0].MapToIPv4();
         _endpointLocal = new IPEndPoint(ipAddressLocal, 5900);
         _endpointLarcTcp = new IPEndPoint(ipAdress, portaTcp);
         _endPointLarcUdp = new IPEndPoint(ipAdress, portaUdp);
     }
-    
+
     public UsuariosDto ObterUsuarios()
     {
-        var socket = new Socket(_endpointLocal.AddressFamily, SocketType.Stream, protocolType: ProtocolType.Tcp);
-        
-        socket.Connect(_endpointLarcTcp);
-        socket.Send(Encoding.UTF8.GetBytes($"GET USERS {USUARIO_APLICACAO}"));
+        using (var socket = new Socket(_endpointLocal.AddressFamily, SocketType.Stream, protocolType: ProtocolType.Tcp))
+        {
+            socket.Connect(_endpointLarcTcp);
+            socket.Send(Encoding.UTF8.GetBytes($"GET USERS {USUARIO_APLICACAO}"));
 
-        var receive = new byte[128];
-        socket.Receive(receive);
-        
-        var retorno = Encoding.UTF8.GetString(receive);
+            var receive = new byte[128];
+            socket.Receive(receive);
 
-        return new UsuariosDto(retorno);
+            var retorno = Encoding.UTF8.GetString(receive);
+
+            return new UsuariosDto(retorno);
+        }
     }
 
     public MensagemRecebidaDto ObterMensagemParaUsuarioPadrao()
     {
-        var socket = new Socket(_endpointLocal.AddressFamily, SocketType.Stream, protocolType: ProtocolType.Tcp);
-        
-        socket.Connect(_endpointLarcTcp);
-        socket.Send(Encoding.UTF8.GetBytes($"GET MESSAGE {USUARIO_APLICACAO}"));
+        using (var socket = new Socket(_endpointLocal.AddressFamily, SocketType.Stream, protocolType: ProtocolType.Tcp))
+        {
+            socket.Connect(_endpointLarcTcp);
+            socket.Send(Encoding.UTF8.GetBytes($"GET MESSAGE {USUARIO_APLICACAO}"));
 
-        var receive = new byte[128];
-        socket.Receive(receive);
-        
-        var retorno = Encoding.UTF8.GetString(receive);
+            var receive = new byte[128];
+            socket.Receive(receive);
 
-        return new MensagemRecebidaDto(retorno);
+            var retorno = Encoding.UTF8.GetString(receive);
+
+            return new MensagemRecebidaDto(retorno);
+        }
     }
 
     public bool EnviarMensagem(EnvioMensagemDto mensagemDto)
