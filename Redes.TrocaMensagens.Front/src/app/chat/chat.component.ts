71728514a09312ec158d921b8f03fd64b4e22ea5
TIEDT <ree_murilo@hotmail.com>
2022-05-28 13:38:11 -0300

    commit antes de fazer merda

--- Redes.TrocaMensagens.Front/src/app/chat/chat.component.ts
+++ Redes.TrocaMensagens.Front/src/app/chat/chat.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {  Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { UsuarioModel } from '../model/usuarios.model';
 import { ChatService } from '../services/chat.service';
+import { interval } from 'rxjs';
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit  {
   usuarioModel: UsuarioModel;
 
   ususariosSelecionados:UsuarioModel;
@@ -17,14 +18,17 @@ export class ChatComponent implements OnInit {
   }
 
   ngOnInit() {
+    // interval(5000).subscribe(() => {
+    //   this.preencheUsuariosOnline();
+    // });
     this.preencheUsuariosOnline();
+  };
+    
+  abrirChat($event: any){
+    console.log($event);
   }
-  
  preencheUsuariosOnline() {
   this.chatService.getTodosUsuarios().subscribe((_usuario: UsuarioModel) => {
-    if(_usuario === undefined){
-      console.log('retornou undefined por isso deu pau')
-    }
       this.usuarioModel = _usuario;
   }, error => {
     this.toastr.error(`Erro ao tentar Usuario: ${error}`);
